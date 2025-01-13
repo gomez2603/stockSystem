f2bf383aa03a523c01d5de21d6b8494f5bd8c96a
Jose Manuel Gomez Leon <jose.gomez@embsoft.com.mx>
2025-01-12 22:15:21 -0600

    feat se agrega nav bar

--- stockSystemFrontend/src/app/app.component.ts
+++ stockSystemFrontend/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component, inject } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
 import { ProductService } from './products.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule,MatCardModule,MatButtonModule],
+  imports: [CommonModule,MatCardModule,MatButtonModule,MatToolbarModule,MatSidenavModule,MatIconModule],
   
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -14,5 +17,8 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
    private readonly productSvsc = inject(ProductService)
    products$ = this.productSvsc.getProducts()
+   showFiller = false;
+   
 
 }
+
